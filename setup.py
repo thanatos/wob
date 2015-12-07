e2dec71c2a796e27932f729310d153d5e4fa47f7
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2015-12-06 21:36:56 -0800

    Add a simple setup.py setup file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Wob',
+    version='0.1',
+    description='Python web framework',
+    author='Roy Wellington Ⅳ',
+    packages=['wob'],
+)
